ee6e3edd6d40a986f4428feb9193b78ff4bdae03
thalisson-sousa <thalissonpop91@gmail.com>
2023-03-24 00:34:25 -0300

    corrigindo casas decimais

--- main.js
+++ main.js
@@ -15,43 +15,37 @@ function calcular() {
 
     if (IMC <= 18.5) {
         let resultado = document.querySelector(".resultado");
-        resultado.innerHTML = `Você está abaixo do peso!
-        seu IMC é: ${IMC}`
+        resultado.innerHTML = `Você está abaixo do peso! seu IMC é: ${IMC.toFixed(2)}`
         revelar()
 
     }else if (IMC > 18.5 && IMC <= 24.9) {
         let resultado = document.querySelector(".resultado");
         let resultadoIMC = document.querySelector(".resultadoIMC");
-        resultado.innerHTML = `Parabéns, Você está no seu peso ideal!`
-        resultadoIMC.innerHTML = `seu IMC é: ${IMC}`
+        resultado.innerHTML = `Parabéns, Você está no seu peso ideal! seu IMC é: ${IMC.toFixed(2)}`
         revelar()
 
     }else if (IMC >= 25.0 && IMC <= 29.9) {
         let resultado = document.querySelector(".resultado");
         let resultadoIMC = document.querySelector(".resultadoIMC");
-        resultado.innerHTML = `Você está acima do peso!`
-        resultadoIMC.innerHTML = `seu IMC é: ${IMC}`
+        resultado.innerHTML = `Você está acima do peso! seu IMC é: ${IMC.toFixed(2)}`
         revelar()
 
     }else if (IMC > 30.0 && IMC <= 34.9) {
         let resultado = document.querySelector(".resultado");
         let resultadoIMC = document.querySelector(".resultadoIMC");
-        resultado.innerHTML = `Você está com Obesidade Classe 1!`
-        resultadoIMC.innerHTML = `seu IMC é: ${IMC}`
+        resultado.innerHTML = `Você está com Obesidade Classe 1! seu IMC é: ${IMC.toFixed(2)}`
         revelar()
 
     }else if (IMC > 35.0 && IMC <= 39.9) {
         let resultado = document.querySelector(".resultado");
         let resultadoIMC = document.querySelector(".resultadoIMC");
-        resultado.innerHTML = `Você está com Obesidade Classe 2!`
-        resultadoIMC.innerHTML = `seu IMC é: ${IMC}`
+        resultado.innerHTML = `Você está com Obesidade Classe 2! seu IMC é: ${IMC.toFixed(2)}`
         revelar()
 
     }else if (IMC > 40.0) {
         let resultado = document.querySelector(".resultado");
         let resultadoIMC = document.querySelector(".resultadoIMC");
-        resultado.innerHTML = `Você está com Obesidade Classe 3!`
-        resultadoIMC.innerHTML = `seu IMC é: ${IMC}`
+        resultado.innerHTML = `Você está com Obesidade Classe 3! seu IMC é: ${IMC.toFixed(2)}`
         revelar()
     }
 }
